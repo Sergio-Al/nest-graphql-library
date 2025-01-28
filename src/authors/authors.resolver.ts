2ab7cf9e5969daf447d440ca74c0df6c137f306e
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2025-01-28 01:14:09 -0400

    Feat. Roles for authors and books

--- src/authors/authors.resolver.ts
+++ src/authors/authors.resolver.ts
@@ -13,15 +13,21 @@ import { CreateAuthorInput } from './dto/create-author.input';
 import { UpdateAuthorInput } from './dto/update-author.input';
 import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
 
 @Resolver(() => Author)
 @UseGuards(JwtAuthGuard)
 export class AuthorsResolver {
   constructor(private readonly authorsService: AuthorsService) {}
 
-  @Mutation(() => Author)
+  @Mutation(() => Author, {
+    description: 'Create a new author, requires a Admin Role',
+  })
   createAuthor(
     @Args('createAuthorInput') createAuthorInput: CreateAuthorInput,
+    @CurrentUser([ValidRoles.admin]) user: User,
   ): Promise<Author> {
     return this.authorsService.create(createAuthorInput);
   }
@@ -38,21 +44,30 @@ export class AuthorsResolver {
     return this.authorsService.findOne(id);
   }
 
-  @Mutation(() => Author)
+  @Mutation(() => Author, {
+    description: 'Update an author, requires a Admin Role',
+  })
   updateAuthor(
     @Args('updateAuthorInput') updateAuthorInput: UpdateAuthorInput,
+    @CurrentUser([ValidRoles.admin]) user: User,
   ): Promise<Author> {
     return this.authorsService.update(updateAuthorInput.id, updateAuthorInput);
   }
 
-  @Mutation(() => Author)
+  @Mutation(() => Author, {
+    description: 'Remove an author, requires a Admin Role',
+  })
   removeAuthor(
     @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @CurrentUser([ValidRoles.admin]) user: User,
   ): Promise<Author> {
     return this.authorsService.remove(id);
   }
 
-  @ResolveField(() => Number, { name: 'totalAuthors' })
+  @ResolveField(() => Number, {
+    name: 'totalAuthors',
+    description: 'Total number of authors',
+  })
   totalAuthors(): Promise<number> {
     return this.authorsService.totalAuthors();
   }
--- src/books/books.resolver.ts
+++ src/books/books.resolver.ts
@@ -3,28 +3,37 @@ import {
   Query,
   Mutation,
   Args,
-  Int,
   ResolveField,
   ID,
-  Parent,
 } from '@nestjs/graphql';
+import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
+
 import { BooksService } from './books.service';
 import { Book } from './entities/book.entity';
+
 import { CreateBookInput } from './dto/create-book.input';
 import { UpdateBookInput } from './dto/update-book.input';
-import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { AddBookAuthorInput } from './dto/add-book-author.input';
 import { AddBookCategoryInput } from './dto/add-book-category.input';
+
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
+
+import { User } from 'src/users/entities/user.entity';
 
 @Resolver(() => Book)
 @UseGuards(JwtAuthGuard)
 export class BooksResolver {
   constructor(private readonly booksService: BooksService) {}
 
-  @Mutation(() => Book)
+  @Mutation(() => Book, {
+    name: 'createBook',
+    description: 'Create a new book, requires a Admin Role',
+  })
   createBook(
     @Args('createBookInput') createBookInput: CreateBookInput,
+    @CurrentUser([ValidRoles.admin]) user: User,
   ): Promise<Book> {
     return this.booksService.create(createBookInput);
   }
@@ -41,31 +50,53 @@ export class BooksResolver {
     return this.booksService.findOne(id);
   }
 
-  @Mutation(() => Book)
+  @Mutation(() => Book, {
+    name: 'updateBook',
+    description: 'Update a book, requires a Admin Role',
+  })
   updateBook(
     @Args('updateBookInput') updateBookInput: UpdateBookInput,
+    @CurrentUser([ValidRoles.admin]) user: User,
   ): Promise<Book> {
     return this.booksService.update(updateBookInput.id, updateBookInput);
   }
 
-  @Mutation(() => Book)
-  removeBook(@Args('id', { type: () => ID }) id: string): Promise<Book> {
+  @Mutation(() => Book, {
+    name: 'removeBook',
+    description: 'Remove a book, requires a Admin Role',
+  })
+  removeBook(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+  ): Promise<Book> {
     return this.booksService.remove(id);
   }
 
-  @Mutation(() => Book)
-  addAuthorsToBook(@Args('input') input: AddBookAuthorInput): Promise<Book> {
+  @Mutation(() => Book, {
+    name: 'addAuthorsToBook',
+    description: 'Add authors to a book, requires a Admin Role',
+  })
+  addAuthorsToBook(
+    @Args('input') input: AddBookAuthorInput,
+    @CurrentUser([ValidRoles.admin]) user: User,
+  ): Promise<Book> {
     return this.booksService.addAuthorsToBook(input);
   }
 
-  @Mutation(() => Book)
+  @Mutation(() => Book, {
+    name: 'addCategoriesToBook',
+    description: 'Add categories to a book, requires a Admin Role',
+  })
   addCategoriesToBook(
     @Args('input') input: AddBookCategoryInput,
+    @CurrentUser([ValidRoles.admin]) user: User,
   ): Promise<Book> {
     return this.booksService.addCategoriesToBook(input);
   }
 
-  @ResolveField(() => Number, { name: 'totalBooks' })
+  @ResolveField(() => Number, {
+    name: 'totalBooks',
+    description: 'Total number of books',
+  })
   totalBooks(): Promise<number> {
     return this.booksService.totalBooks();
   }
